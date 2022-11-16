58e38a2b5499d49dfaf26b938294b70cf66dc470
Magdalena Brodala <magdalena.brodala.programista@gmail.com>
2022-11-16 16:52:10 +0100

    feature__setup_login_at_begining

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ using namespace std;
 
 int main()
 {
+
+
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt");
     //ksiazkaAdresowa.rejestracjaUzytkownika();
     //ksiazkaAdresowa.rejestracjaUzytkownika();
     ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    ksiazkaAdresowa.logowanieUzytkownika();
 
     return 0;
 }
